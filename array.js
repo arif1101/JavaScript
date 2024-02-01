3c9949cf7efff4b8be72fee2e1f697b6bcbc9f68
arif1101 <arifurrahmanarif223@gmail.com>
2024-02-01 15:31:09 +0600

    javascript

---
+++ array.js
@@ -0,0 +1,39 @@
+const number = [12, 24 ,32, 43, 56, 67];
+console.log(number);
+
+// array length
+console.log("size : ",number.length);
+
+// array push
+number.push(44);
+number.push(80);
+number.push(100,101,102);
+console.log("after push : ",number);
+
+// pop array
+
+number.pop();
+console.log("after pop : ",number);
+
+
+// while loop
+
+console.log("while loop");
+
+let i=0;
+while(i < 60){
+    console.log(i+1);
+    i++;
+}
+
+// for loop
+console.log("for loop");
+let n=10;
+for (let i=1;i<n;i++){
+    if(i%2==0){
+        console.log(i);
+    }
+}
+for (let i=1;i<n;i++){
+    
+}
