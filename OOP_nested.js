fd06f01defe775675fea8fb8726c644d16f72c9c
arif1101 <arifurrahmanarif223@gmail.com>
2024-02-01 17:44:14 +0600

    colmpalte keys,values,nested

---
+++ OOP_nested.js
@@ -0,0 +1,15 @@
+const college = {
+    name: 'RnC',
+    class: ['11', '12'],
+    event: ['footbal', 'cricket', 'badminton'],
+    unique: {
+        color: 'blue',
+        result: {
+            gpa: 5,
+            merit: 'top',
+        }
+    }
+}
+
+console.log(college.name);
+console.log(college.unique.result.gpa);
