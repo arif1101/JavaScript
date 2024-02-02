0b8f811226d2acd4eac689636bab6e63d230a90b
arif1101 <arifurrahmanarif223@gmail.com>
2024-02-02 20:40:25 +0600

    array in function

---
+++ function_array.js
@@ -0,0 +1,12 @@
+function sumOfNumber(numbers){
+    let fnSum=0;
+    for(number of numbers){
+        fnSum = fnSum+number;
+        
+    }
+    return fnSum;
+}
+
+const numbs= [10,20,30,40];
+const sum = sumOfNumber(numbs);
+console.log('sum of number is : ', sum);
