91253dfc2b94237a9249a27fdb552bcd980c8517
arif1101 <arifurrahmanarif223@gmail.com>
2024-02-02 20:22:50 +0600

    function

---
+++ function.js
@@ -0,0 +1,29 @@
+function vatKhau(){
+    console.log('hand wahs kore aso');
+    console.log('boso');
+    console.log('plate e khabar dhabar nau');
+    console.log('gopat gopat kore khau');
+}
+
+function square(number){
+    const borgo = number*number;
+    console.log('square of the given number is : ',borgo);
+}
+
+square(4);
+console.log('-----------');
+square(5);
+console.log('------------');
+square(6);
+console.log('-------------');
+
+
+
+function add(num1,num2){
+    const sum = num1+num2;
+    console.log('sum of the given number is : ', sum);
+}
+
+add(10,12);
+console.log('-------------');
+add(45,55);
